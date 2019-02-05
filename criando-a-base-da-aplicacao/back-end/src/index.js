4b75b298d3280f03923c37e8ed68337f0f049ab2
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-05 21:19:47 -0200

    :construction: Adicionando conexao e rotas

--- criando-a-base-da-aplicacao/back-end/src/index.js
+++ criando-a-base-da-aplicacao/back-end/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello world');
+mongoose.connect('mongodb://goweek:goweek123@ds033337.mlab.com:33337/goweek-backend', {
+  useNewUrlParser: true,
 });
+
+app.use(require('./routes'));
 
 app.listen(3000, () => {
   console.log('Server stared on port 3000');
