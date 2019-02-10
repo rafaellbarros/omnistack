8ff33b8bfab2b7ff1d283ea6db8e27103bb2beb9
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-09 23:04:02 -0200

    :construction: Criando routes

---
+++ 03-desenvolvendo-app-mobile/omnistackrn/src/routes.js
@@ -0,0 +1,13 @@
+import { createSwitchNavigator, createAppContainer} from 'react-navigation';
+
+import Login from './pages/Login';
+import Timeline from './pages/Timeline';
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Login,
+        Timeline 
+    })
+);
+
+export default Routes;
