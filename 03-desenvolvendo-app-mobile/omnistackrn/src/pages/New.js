66a4e5bf1b3fc15f7ce8ac5d1d628f7acaa2d901
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-10 21:03:46 -0200

    :construction: Criando page New

---
+++ 03-desenvolvendo-app-mobile/omnistackrn/src/pages/New.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+import { 
+    SafeAreaView, 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    TextInput,
+    AsyncStorage, 
+    StyleSheet 
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default class New extends Component {
+  
+  static navigationOptions = { 
+      header: null
+  };
+  
+  state = {
+      newTweet: '',
+  };
+
+  render() {
+    return (
+        <SafeAreaView style={styles.container} >
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => {}} >
+                    <Icon name="close" size={24} color="#4BB0EE" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => {}} >
+                    <Text style={styles.buttonText}>Tweetar</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#FFF"
+    },
+  
+    header: {
+      paddingTop: 10,
+      paddingHorizontal: 20,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    },
+  
+    button: {
+      height: 32,
+      paddingHorizontal: 20,
+      borderRadius: 16,
+      backgroundColor: "#4BB0EE",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+  
+    buttonText: {
+      color: "#FFF",
+      fontSize: 16,
+      fontWeight: "bold"
+    },
+  
+    input: {
+      margin: 20,
+      fontSize: 16,
+      color: "#333"
+    }
+  });
+  
