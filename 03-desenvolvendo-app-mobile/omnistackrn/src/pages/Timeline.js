d6ea1b814bdf68647b7f54d51d80701c646813e2
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-10 15:59:24 -0200

    :construction: Implementando Timeline

--- 03-desenvolvendo-app-mobile/omnistackrn/src/pages/Timeline.js
+++ 03-desenvolvendo-app-mobile/omnistackrn/src/pages/Timeline.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
-import { View } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-// import styles from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class pages extends Component {
+  static navigationOptions = {
+    title: 'Início',
+    headerRight: (
+      <TouchableOpacity onPress={() => {}}>
+        <Icon
+          style={{ marginRight: 20}} 
+          name='add-circle-outline'
+          size={24}
+          color='#4BB0EE'
+        />
+      </TouchableOpacity>
+    ),
+    headerTitleStyle: { 
+      textAlign:'center', 
+      flex:1 
+    },
+  };
+
+  state = {
+    tweets: []
+,  }
+
+  // onInit
+  async componentDidMount() {
+     const response = await api.get('tweets');
+     this.setState({
+        tweets: response.data
+     });
+  }
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+       { this.state.tweets.map(tweet => <Text>{tweet.author}</Text>)}
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
+
