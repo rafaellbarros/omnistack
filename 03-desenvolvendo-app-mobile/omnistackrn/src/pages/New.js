ca109baffd910c037d5a30f615690024c9f05860
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-10 21:24:40 -0200

    :construction: Implementando TextInput

--- 03-desenvolvendo-app-mobile/omnistackrn/src/pages/New.js
+++ 03-desenvolvendo-app-mobile/omnistackrn/src/pages/New.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import { 
     SafeAreaView, 
@@ -19,20 +20,48 @@ export default class New extends Component {
   };
   
   state = {
-      newTweet: '',
+      newTweet: "",
   };
+
+  goBack = () => {
+      this.props.navigation.pop();
+  }
+
+  handleTweet = async () => {
+    const content = this.state.newTweet;
+    const author = await AsyncStorage.getItem('@OmniStack:username');
+    
+    await api.post('tweets', { author, content });
+
+    this.goBack();
+  }
+
+  handleInputChange = (newTweet) => {
+      this.setState({ newTweet });
+  }
 
   render() {
     return (
         <SafeAreaView style={styles.container} >
             <View style={styles.header}>
-                <TouchableOpacity onPress={() => {}} >
+                <TouchableOpacity onPress={this.goBack} >
                     <Icon name="close" size={24} color="#4BB0EE" />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => {}} >
+                <TouchableOpacity style={styles.button} onPress={this.handleTweet} >
                     <Text style={styles.buttonText}>Tweetar</Text>
                 </TouchableOpacity>
             </View>
+
+            <TextInput 
+                style={styles.input}
+                multiline
+                placeholder="O que está acontecendo?"
+                placeholderTextColor="#999"
+                value={this.state.newTweet}
+                onChangeText={this.handleInputChange}
+                returnKeyType="send"
+                onSubmitEditing={this.handleTweet}
+            />
         </SafeAreaView>
     );
   }
